06c3886888a9730b265fe901590142fd927c15ca
Joe Snyder <snyderjosephd@yahoo.com>
2024-12-18 21:59:16 -0600

    Update client and server socket scripts to use socket instead of pybluez

--- iot-lab-2/frontend_tutorial/bt_client.py
+++ iot-lab-2/frontend_tutorial/bt_client.py
@@ -1,18 +1,38 @@
-import bluetooth
+import socket
 
-host = "D8:3A:DD:70:92:5D" # The address of Raspberry PI Bluetooth adapter on the server.
-port = 1
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((host, port))
-while 1:
-    text = input("Enter your message: ") # Note change to the old (Python 2) raw_input
-    if text == "quit":
-        break
-    sock.send(text.encode("utf-8"))
+host = "D8:3A:DD:70:92:5D"  # Update with the server's Bluetooth MAC address
+port = 1  # RFCOMM port
 
+try:
+    sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    sock.connect((host, port))
+    print("Connected successfully!")
+
+    # Test sending data
+    sock.send("Hello, Server!".encode("utf-8"))
+
+    # Receive response
     data = sock.recv(1024).decode("utf-8")
-    print("from server: ", data)
+    print("From server:", data)
 
-sock.close()
+    sock.close()
+except Exception as e:
+    print("Bluetooth error:", e)
 
+# import bluetooth
+
+# host = "D8:3A:DD:70:92:5D" # The address of Raspberry PI Bluetooth adapter on the server.
+# port = 1
+# sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+# sock.connect((host, port))
+# while 1:
+#     text = input("Enter your message: ") # Note change to the old (Python 2) raw_input
+#     if text == "quit":
+#         break
+#     sock.send(text.encode("utf-8"))
+
+#     data = sock.recv(1024).decode("utf-8")
+#     print("from server: ", data)
+
+# sock.close()
 
--- iot-lab-2/frontend_tutorial/bt_server.py
+++ iot-lab-2/frontend_tutorial/bt_server.py
@@ -1,23 +1,53 @@
-import bluetooth
+import socket
 
-hostMACAddress = "D8:3A:DD:70:92:5D" # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-port = 0
+# The address of Raspberry Pi bluetooth adapter on the server. The server might have multiple bluetooth adapters
+hostMACAddress = "D8:3A:DD:70:92:5D"
+port = 1 # Port must be set explicitly (typically 1 for Bluetooth RFCOMM)
 backlog = 1
 size = 1024
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.bind((hostMACAddress, port))
-s.listen(backlog)
-print("listening on port ", port)
-try:
-    client, clientInfo = s.accept()
-    while 1:   
-        print("server recv from: ", clientInfo)
-        data = client.recv(size)
-        if data:
-            print(data.decode("utf-8"))
-            client.send(data.encode("utf-8")) # Echo back to client
-except: 
-    print("Closing socket")
-    client.close()
-    s.close()
 
+# Create a bluetooth socket
+server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+server_sock.bind((hostMACAddress, port))
+server_sock.listen(backlog)
+
+print(f"Listening on {hostMACAddress}, port {port}")
+
+try:
+    client_sock, client_info = server_sock.accept()
+    print(f"Accepted connection from {client_info}")
+
+    while True:
+        data = client_sock.recv(size)
+        if data:
+            print("Received: ", data.decode("utf-8"))
+            client_sock.send(data) # Echo back the received data
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    print("Closing sockets")
+    client_sock.close()
+    server_sock.close()
+
+# import bluetooth
+
+# hostMACAddress = "D8:3A:DD:70:92:5D" # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+# port = 0
+# backlog = 1
+# size = 1024
+# s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+# s.bind((hostMACAddress, port))
+# s.listen(backlog)
+# print("listening on port ", port)
+# try:
+#     client, clientInfo = s.accept()
+#     while 1:   
+#         print("server recv from: ", clientInfo)
+#         data = client.recv(size)
+#         if data:
+#             print(data.decode("utf-8"))
+#             client.send(data.encode("utf-8")) # Echo back to client
+# except: 
+#     print("Closing socket")
+#     client.close()
+#     s.close()
